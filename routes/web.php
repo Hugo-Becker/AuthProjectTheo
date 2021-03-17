44eeaa8fe7a94d31335779e19a4de5119da47d44
Hugo <becker.hugo@hotmail.com>
2021-03-17 20:54:11 +0100

    s2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\AvatarController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +22,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('avatars',AvatarController::class);
+Route::resource('users',UserController::class);
+Route::resource('images',ImageController::class);
+Route::resource('categories',CategoryController::class);
+
+Auth::routes();
+
+
+// Route::get('/register',[RegisterController::class,'index']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
